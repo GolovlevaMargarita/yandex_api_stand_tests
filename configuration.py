8cbc1831959bb3b8b7bbc0db172b56682b03f4a3
Golovleva Margarita <golovleva.margari@yandex.ru>
2023-04-30 14:55:35 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://46f507e8-bf84-4f6c-8ecb-84d03c2cb31d.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
